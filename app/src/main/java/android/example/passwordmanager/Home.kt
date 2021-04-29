a77ae7254fbd5d9e2096003357e8769bdfc5e6f9
Kiwiegg <larrywutoronto@gmail.com>
2021-04-28 21:52:51 -0400

    home/login

--- app/src/main/java/android/example/passwordmanager/Home.kt
+++ app/src/main/java/android/example/passwordmanager/Home.kt
@@ -1,29 +1,38 @@
 package android.example.passwordmanager
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setGradient()
     }
 
-    fun passCheck(view: View) {
-        val passwordText = findViewById<EditText>(R.id.homePassword)
-        val passwordTitle = findViewById<TextView>(R.id.submitPassword)
-        if(passwordText.getText().toString() == "test") {
-            passwordText.setText("")
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            passwordTitle.text = "Log In"
-        }else{
-            passwordText.setText("")
-            passwordTitle.text = "Try Again!"
-        }
+    private fun setGradient() {
+        val appName = findViewById<TextView>(R.id.appName)
+        val textShader = LinearGradient(
+            0.toFloat(), 0.toFloat(), appName.width.toFloat(), appName.textSize,
+            Color.parseColor("#EECDA3"), Color.parseColor("#EF629F"), Shader.TileMode.CLAMP
+        )
+        appName.paint.setShader(textShader)
+    }
+
+    fun loginWindow(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
