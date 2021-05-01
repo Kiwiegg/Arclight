51f292453d51921b6a3e7100e16b6e96bb02a2b9
Kiwiegg <larrywutoronto@gmail.com>
2021-05-01 17:30:36 -0400

    added login system

--- app/src/main/java/android/example/passwordmanager/LoginActivity.kt
+++ app/src/main/java/android/example/passwordmanager/LoginActivity.kt
@@ -3,9 +3,16 @@ package android.example.passwordmanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,17 +37,40 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
             return
         }
-        if (checkForValidLogin(username, password)) {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        } else {
-            Toast.makeText(this, "Invalid Username or Incorrect Password", Toast.LENGTH_SHORT).show()
-            passwordText.setText("")
-            usernameText.setText("")
-        }
+        checkForValidLogin(username, password)
+        passwordText.setText("")
+        usernameText.setText("")
     }
 
-    private fun checkForValidLogin(username: String, password: String): Boolean {
-        return username == "login" && password == "password"
+    private fun checkForValidLogin(username: String, password: String) {
+        val users = Firebase.database.getReference("Users")
+        val user = users.orderByChild("username").equalTo(username)
+        // Log.d("fbTest", "ruunning")
+        user.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                // empty
+            }
+
+            override fun onDataChange(data: DataSnapshot) {
+                Log.d("test", "The data is $data")
+                processData(username, password, data)
+            }
+        })
+    }
+
+    private fun processData(username: String, password: String, arr: DataSnapshot) {
+        if (!arr.hasChildren()) {
+            Toast.makeText(this, "The username does not exist", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val data = arr.children.iterator().next()
+        val realPassword = data.child("password").value
+        if (password == realPassword) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("username", "username")
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/android/example/passwordmanager/signupActivity.kt
+++ app/src/main/java/android/example/passwordmanager/signupActivity.kt
@@ -1,8 +1,18 @@
 package android.example.passwordmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class signupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +22,57 @@ class signupActivity : AppCompatActivity() {
 
     fun homeScreen(view: View) {
         finish()
+    }
+
+    fun submitSignup (view: View) {
+        val usernameText = findViewById<EditText>(R.id.usernameText)
+        val passwordText = findViewById<EditText>(R.id.passwordText)
+        val confirmPasswordText = findViewById<EditText>(R.id.confirmPassword)
+        val username = usernameText.text.toString()
+        val password = passwordText.text.toString()
+        val confirmPassword = confirmPasswordText.text.toString()
+        if (username == "") {
+            Toast.makeText(this, "Please enter an username", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password == "") {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password != confirmPassword) {
+            Toast.makeText(this, "The passwords you have entered do not match", Toast.LENGTH_SHORT).show()
+            return
+        }
+        checkForValidLogin(username, password)
+        usernameText.setText("")
+    }
+
+    private fun checkForValidLogin(username: String, password: String) {
+        val users = Firebase.database.getReference("Users")
+        val user = users.orderByChild("username").equalTo(username)
+        user.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                // empty
+            }
+
+            override fun onDataChange(data: DataSnapshot) {
+                Log.d("test", "The data is $data")
+                processData(username, password, data, users)
+            }
+        })
+    }
+
+    private fun processData(username: String, password: String, arr: DataSnapshot, users: DatabaseReference) {
+        if (arr.hasChildren()) {
+            Toast.makeText(this, "The username is already taken, please enter a new onm", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // val newUser = User(username, password)
+        val newUser = users.push()
+        newUser.child("username").setValue(username)
+        newUser.child("password").setValue(password)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("username", "username")
+        startActivity(intent)
     }
 }
